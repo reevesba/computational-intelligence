4487935d5580faa68f0f2b3534cb04ac7883a45c
reevesba <reevesbra@outlook.com>
2021-04-27 20:12:22 -0700

    adding results

--- projects/project2/src/mlp.py
+++ projects/project2/src/mlp.py
@@ -1,7 +1,7 @@
 ''' Multi Layer Perceptron
 
 Author: Bradley Reeves, Sam Shissler
-Date:   04/21/2021
+Date:   04/27/2021
 
 Code adapted from Chapter 4 of Machine Learning: An Algorithmic 
 Perspective (2nd Edition) by Stephen Marsland (http://stephenmonika.net) 
@@ -11,7 +11,7 @@ import numpy as np
 from scipy.special import expit as logistic_sigmoid
 
 class MLP:
-    def __init__(self, X_train, y_train, hidden_layer_sizes=(100,), beta=-1, momentum=0.9, out_type='logistic'):
+    def __init__(self, X_train, y_train, hidden_layer_sizes=(100,), beta=-1, momentum=0.9, activation='logistic'):
         ''' Initialize MLP instance
             ----------
             self : object
@@ -33,27 +33,24 @@ class MLP:
             None
         '''
         self.n_samples, self.n_features = X_train.shape
-        self.n_outputs = y_train.shape[1]
         self.beta = beta
         self.momentum = momentum
-        self.out_type = out_type
-        self.layer_units = ([self.n_features] + list(hidden_layer_sizes) + [self.n_outputs])
+        self.activation = activation
+        self.layer_units = ([self.n_features] + list(hidden_layer_sizes) + [y_train.shape[1]])
 
         # Setup hidden layers/nodes
-        self.n_hidden_layers = len(hidden_layer_sizes)
-        if self.n_hidden_layers < 1:
-            raise Exception("Must have at least one hidden layer.")
+        if len(self.layer_units) - 2 < 1:
+            raise ValueError("Must have at least one hidden layer.")
 
-        if self.n_hidden_layers > 2:
-            raise Exception("Maximum 2 hidden layers allowed.")
+        if len(self.layer_units) - 2 > 2:
+            raise ValueError("Maximum 2 hidden layers allowed.")
 
-        if self.n_hidden_layers == 1:
-            # He initialization
+        # Initialize weights using He et al. method
+        if len(self.layer_units) - 2 == 1:
             self.weights1 = (np.random.rand(self.layer_units[0] + 1, self.layer_units[1]))*np.sqrt(2/self.layer_units[0])
             self.weights2 = (np.random.rand(self.layer_units[1] + 1, self.layer_units[2]))*np.sqrt(2/self.layer_units[1])
         
-        if self.n_hidden_layers == 2:
-            # He initialization
+        if len(self.layer_units) - 2 == 2:
             self.weights1 = (np.random.rand(self.layer_units[0] + 1, self.layer_units[1]))*np.sqrt(2/self.layer_units[0])
             self.weights2 = (np.random.rand(self.layer_units[1] + 1, self.layer_units[2]))*np.sqrt(2/self.layer_units[1])
             self.weights3 = (np.random.rand(self.layer_units[2] + 1, self.layer_units[3]))*np.sqrt(2/self.layer_units[2])
@@ -121,49 +118,70 @@ class MLP:
             None
         '''
         X_train = np.concatenate((X_train, -np.ones((self.n_samples, 1))), axis=1)
-    
-        updatew1 = np.zeros((np.shape(self.weights1)))
-        updatew2 = np.zeros((np.shape(self.weights2)))
-        if self.n_hidden_layers == 2: 
-            updatew3 = np.zeros((np.shape(self.weights3)))
             
         for n in range(epochs):
+            # Run network forward and calculate output error
             self.outputs = self.feed_forward(X_train)
 
-            error = 0.5*np.sum((self.outputs - y_train)**2)
-            if (np.mod(n, 100) == 0):
-                if trace: print("Iteration: ", n, " Error: ", error)    
+            if trace:
+                error = 0.5*np.sum((self.outputs - y_train)**2)
+                if (np.mod(n, 100) == 0):
+                    print("Iteration: ", n, " Error: ", error)    
 
-            # Different types of output neurons
-            if self.out_type == 'linear':
+            # Output Errors
+            if self.activation == 'linear':
             	deltao = (self.outputs - y_train)/self.n_samples
-            elif self.out_type == 'logistic':
+            elif self.activation == 'logistic':
             	deltao = self.beta*(self.outputs - y_train)*self.outputs*(1.0 - self.outputs)
-            elif self.out_type == 'softmax':
+            elif self.activation == 'softmax':
                 deltao = (self.outputs - y_train)*(self.outputs*(-self.outputs) + self.outputs)/self.n_samples 
             else:
-            	print("error")
+            	raise ValueError("No activation function specified.")
             
-            # Back propagate
-            if self.n_hidden_layers == 1:
-                deltah = self.hidden1*self.beta*(1.0 - self.hidden1)*(np.dot(deltao, np.transpose(self.weights2)))
+            self.backpropagate(X_train, deltao, eta)
 
-                updatew1 = eta*(np.dot(np.transpose(X_train), deltah[:, :-1])) + self.momentum*updatew1
-                updatew2 = eta*(np.dot(np.transpose(self.hidden1), deltao)) + self.momentum*updatew2
+    def backpropagate(self, X_train, deltao, eta):
+        ''' Propagate errors backwards through network
+            ----------
+            self : object
+                MLP instance
+            X_train : 2d numpy array
+                Collection of input vectors x₁, x₂, ..., xₙ
+            deltao : 2d numpy array
+                Output layer errors
+            eta : float
+                Learning rate 
+            Returns
+            -------
+            None
+        '''
+        updatew1 = np.zeros((np.shape(self.weights1)))
+        updatew2 = np.zeros((np.shape(self.weights2)))
 
-                self.weights1 -= updatew1
-                self.weights2 -= updatew2
-                      
-            if self.n_hidden_layers == 2:
-                deltah = self.hidden2*self.beta*(1.0 - self.hidden2)*(np.dot(deltao, np.transpose(self.weights3)))
+        # One hidden layer
+        if len(self.layer_units) - 2 == 1:
+            deltah1 = self.hidden1*self.beta*(1.0 - self.hidden1)*(np.dot(deltao, np.transpose(self.weights2)))
 
-                updatew1 = eta*(np.dot(np.transpose(X_train), deltah[:, :-1])) + self.momentum*updatew1
-                updatew2 = eta*(np.dot(np.transpose(self.hidden1), deltao)) + self.momentum*updatew2
-                updatew3 = eta*(np.dot(np.transpose(self.hidden2), deltao)) + self.momentum*updatew3
+            updatew1 = eta*(np.dot(np.transpose(X_train), deltah1[:, :-1])) + self.momentum*updatew1
+            updatew2 = eta*(np.dot(np.transpose(self.hidden1), deltao)) + self.momentum*updatew2
 
-                self.weights1 -= updatew1
-                self.weights2 -= updatew2
-                self.weights3 -= updatew3
+            self.weights1 -= updatew1
+            self.weights2 -= updatew2
+                    
+        # Two hidden layers
+        if len(self.layer_units) - 2 == 2:
+            updatew3 = np.zeros((np.shape(self.weights3)))
+
+            deltah2 = self.hidden2*self.beta*(1.0 - self.hidden2)*(np.dot(deltao, np.transpose(self.weights3)))
+            deltah1 = self.hidden1*self.beta*(1.0 - self.hidden1)*(np.dot(deltah2[:, :-1], np.transpose(self.weights2)))
+
+            updatew1 = eta*(np.dot(np.transpose(X_train), deltah1[:, :-1])) + self.momentum*updatew1
+            updatew2 = eta*(np.dot(np.transpose(self.hidden1), deltah2[:, :-1])) + self.momentum*updatew2
+            updatew3 = eta*(np.dot(np.transpose(self.hidden2), deltao)) + self.momentum*updatew3
+
+            self.weights1 -= updatew1
+            self.weights2 -= updatew2
+            self.weights3 -= updatew3
             
     def feed_forward(self, inputs):
         ''' Run the network forward
@@ -177,14 +195,14 @@ class MLP:
             2d numpy array
                 Results after activation
         '''
-        if self.n_hidden_layers == 1:
+        if len(self.layer_units) - 2 == 1:
             self.hidden1 = np.dot(inputs, self.weights1)
             self.hidden1 = logistic_sigmoid(self.hidden1)
             self.hidden1 = np.concatenate((self.hidden1, -np.ones((np.shape(inputs)[0], 1))), axis=1)
 
             outputs = np.dot(self.hidden1, self.weights2)
 
-        if self.n_hidden_layers == 2:
+        if len(self.layer_units) - 2 == 2:
             self.hidden1 = np.dot(inputs, self.weights1)
             self.hidden1 = logistic_sigmoid(self.hidden1)
             self.hidden1 = np.concatenate((self.hidden1, -np.ones((np.shape(inputs)[0], 1))), axis=1)
@@ -195,13 +213,48 @@ class MLP:
 
             outputs = np.dot(self.hidden2, self.weights3)
 
-        # Different types of output neurons
-        if self.out_type == 'linear':
+        # Output activations
+        if self.activation == 'linear':
         	return outputs
-        elif self.out_type == 'logistic':
+        elif self.activation == 'logistic':
             return logistic_sigmoid(outputs)
-        elif self.out_type == 'softmax':
+        elif self.activation == 'softmax':
             normalisers = np.sum(np.exp(outputs), axis=1)*np.ones((1, np.shape(outputs)[0]))
             return np.transpose(np.transpose(np.exp(outputs))/normalisers)
         else:
-            print("error")
+            raise ValueError("No activation function specified.")
+
+    def confmat(self, inputs, targets):
+        ''' Confustion matrix for classification
+            ----------
+            self : object
+                MLP instance
+            inputs : 2d numpy array
+                Collection of input vectors x₁, x₂, ..., xₙ
+            targets : 2d numpy array
+                Supervised learning labels
+            Returns
+            -------
+            None
+        '''
+        inputs = np.concatenate((inputs, -np.ones((np.shape(inputs)[0], 1))), axis=1)
+        outputs = self.feed_forward(inputs)
+        
+        nclasses = np.shape(targets)[1]
+
+        if nclasses==1:
+            nclasses = 2
+            outputs = np.where(outputs > 0.5, 1, 0)
+        else:
+            # 1-of-N encoding
+            outputs = np.argmax(outputs, 1)
+            targets = np.argmax(targets, 1)
+
+        cm = np.zeros((nclasses, nclasses))
+        for i in range(nclasses):
+            for j in range(nclasses):
+                cm[i, j] = np.sum(np.where(outputs == i, 1, 0)*np.where(targets == j, 1, 0))
+
+        print("Confusion matrix is:")
+        print(cm)
+        print("Percentage Correct: ", np.trace(cm)/np.sum(cm)*100)
